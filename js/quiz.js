bd467358325b46045ba4a2f88cded8e77e362b31
Jacob Lundberg <jacob.lundberg@Jacobs-MacBook-Pro.local>
2019-08-20 14:42:41 +0200

    Randomized names

--- js/quiz.js
+++ js/quiz.js
@@ -6,7 +6,13 @@ function qSA(selector){
     return document.querySelectorAll(selector)
 }
 
-console.log(qS('#answers'))
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
 
 firestore = firebase.firestore();
 
@@ -24,21 +30,42 @@ database.collection('students').get().then((snap) => {
         students.push(snap.data());
         remainingGuesses.push(snap.data());
     })
-    console.log(students);
-    console.log(remainingGuesses);
 }).then( () => {
     newGuess();
 })
 
+//
 function newGuess(){
-    guess = remainingGuesses[Math.floor(Math.random() * remainingGuesses.length)];
-    console.log(guess)
+    //Randomize next guess
+    let guessIndex = Math.floor(Math.random() * remainingGuesses.length);
+    guess = remainingGuesses[guessIndex];
+    console.log(guess);
+    remainingGuesses.splice(guessIndex, 1);
+
+    //Update shown image
     let guessRef = storage.ref('img/' + guess.img + '.jpg');
-    console.log(guessRef)
     guessRef.getDownloadURL().then( (url) => {
-        console.log(url)
-        qS('section#image > img').src = url
+        qS('section#image > img').src = url;
     })
+
+    //Randomize available answers
+    let answers = [];
+    let availableGuesses = remainingGuesses;
+    for(i=0;i<6;i++){
+        guessIndex = Math.floor(Math.random()*availableGuesses.length);
+        answers.push(availableGuesses[guessIndex]);
+        availableGuesses.splice(guessIndex, 1);
+    }
+    answers.push(guess);
+    answers = shuffle(answers)
+
+    answers.forEach( (answer) => {
+        qS('#answers').insertAdjacentHTML('beforeend',
+        `<div>
+            <p>${answer.name}</p>
+        </div>`)
+    })
+    console.log(answers);
 }
 
 var storageRef = storage.ref("googleicon.jpg");
