5e0b781986dd026b0304dd7195d787864cb7a288
Jacob Lundberg <jacob.lundberg@Jacobs-MacBook-Pro.local>
2019-08-20 12:02:01 +0200

    Fixed Firebase Initialization

--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,23 @@
 firestore = firebase.firestore();
-const settings = {timestampsInSnapshots: true};
-firestore.settings(settings);
 
-//Set up database reference
+//Set up database/storage reference
 const database = firebase.firestore();
+const storage = firebase.storage();
+
 firebase.auth().signInAnonymously().catch(error => console.error(error))
+
+database.collection('students').get().then((snap) => {
+    snap.forEach((snap) => {
+        console.log(snap.data())
+    })
+});
+
+var storageRef = storage.ref("googleicon.jpg");
+
+storageRef.getDownloadURL().then( function(url) {
+    console.log(url);
+});
+
+storageRef.getDownloadURL().then( (url) => {
+    console.log(url);
+});
