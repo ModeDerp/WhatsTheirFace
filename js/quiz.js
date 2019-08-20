5330f10673c0a4e8209d30b4112b7b0654782dda
Jacob Lundberg <jacob.lundberg@Jacobs-MacBook-Pro.local>
2019-08-20 14:01:09 +0200

    Quiz starter

--- js/quiz.js
+++ js/quiz.js
@@ -1,22 +1,47 @@
+function qS(selector){
+    return document.querySelector(selector)
+}
+
+function qSA(selector){
+    return document.querySelectorAll(selector)
+}
+
+console.log(qS('#answers'))
+
 firestore = firebase.firestore();
 
 //Set up database/storage reference
 const database = firebase.firestore();
 const storage = firebase.storage();
 
-firebase.auth().signInAnonymously().catch(error => console.error(error))
+firebase.auth().signInAnonymously().catch(error => console.error(error));
 
+let students = [], remainingGuesses = [], guess = {}
+
+//Gather all students from firebase
 database.collection('students').get().then((snap) => {
     snap.forEach((snap) => {
-        console.log(snap.data())
+        students.push(snap.data());
+        remainingGuesses.push(snap.data());
     })
-});
+    console.log(students);
+    console.log(remainingGuesses);
+}).then( () => {
+    newGuess();
+})
+
+function newGuess(){
+    guess = remainingGuesses[Math.floor(Math.random() * remainingGuesses.length)];
+    console.log(guess)
+    let guessRef = storage.ref('img/' + guess.img + '.jpg');
+    console.log(guessRef)
+    guessRef.getDownloadURL().then( (url) => {
+        console.log(url)
+        qS('section#image > img').src = url
+    })
+}
 
 var storageRef = storage.ref("googleicon.jpg");
-
-storageRef.getDownloadURL().then( function(url) {
-    console.log(url);
-});
 
 storageRef.getDownloadURL().then( (url) => {
     console.log(url);
