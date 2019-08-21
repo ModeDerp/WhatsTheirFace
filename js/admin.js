1ba144184a344c38f6c41378c5b614427971a492
Jacob Lundberg <jacob.lundberg@Jacobs-MacBook-Pro.local>
2019-08-21 15:25:41 +0200

    Initiated admin js

---
+++ js/admin.js
@@ -0,0 +1,15 @@
+function qS(selector){
+    return document.querySelector(selector)
+}
+
+function qSA(selector){
+    return document.querySelectorAll(selector)
+}
+
+firestore = firebase.firestore();
+
+//Set up database/storage reference
+const database = firebase.firestore();
+const storage = firebase.storage();
+
+firebase.auth().signInAnonymously().catch(error => console.error(error));
