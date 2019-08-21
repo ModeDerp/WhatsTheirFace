d707c4aca3e03dd4587819433ab8c5efc4e7aaaf
Mattis Abrahamsson <modederp@hotmail.com>
2019-08-21 15:51:15 +0200

    Implemented basic image upload

--- js/admin.js
+++ js/admin.js
@@ -13,3 +13,19 @@ const database = firebase.firestore();
 const storage = firebase.storage();
 
 firebase.auth().signInAnonymously().catch(error => console.error(error));
+
+function loadFile(event) {
+    var output = document.getElementById('previewImg');
+    output.src = URL.createObjectURL(event.target.files[0]);
+
+    var ref = storage.ref('img/hej.jpg');
+
+    var file = event.target.files[0];
+    ref.put(file).then(function(snapshot) {
+        console.log('Uploaded a blob or file!');
+    });
+
+
+};
+
+
