4fc63d028bf3c0744512c55891189d01777a1d48
Jacob Lundberg <jacob.lundberg@Jacobs-MacBook-Pro.local>
2019-08-22 14:43:35 +0200

    Updated student creator (references)

--- js/admin.js
+++ js/admin.js
@@ -14,29 +14,44 @@ const storage = firebase.storage();
 
 firebase.auth().signInAnonymously().catch(error => console.error(error));
 
-function uploadFile(event) {
+function updateImg(){
     var output = document.getElementById('previewImg');
     output.src = URL.createObjectURL(qS('#imgUpload').files[0]);
+}
 
+function uploadFile(event) {
     //Get unique timeId
     let time = getDate();
 
+    //Get all input values
     let name = qS('input#studentname').value
     let group = qS('input#studentgroup').value
     let hobby = qS('textarea#studenthobby').value
     let fileName = group.toUpperCase() + name.toLowerCase() + time
+    let groupPath
 
-    var ref = storage.ref('img/' + fileName + '.jpg');
-    var file = qS('#imgUpload').files[0];
-    ref.put(file).then(() => {
-        alert('Student succesfully added')
-        database.collection('students').doc(name + time).set({
-            name: name,
-            img: fileName,
-            group: group.toUpperCase(),
-            hobby: hobby
-        })
-    });
+    //Find documents containing input group
+    database.collection('groups').where('group', '==', `${group.toUpperCase()}`).get().then((snap) => {
+        snap.forEach((snapshot) => {
+            //Get id from found document
+            groupPath = snapshot.id
+        });
+    }).then(() => {
+        //Create references to group document and image in storage
+        groupRef = database.collection('groups').doc(groupPath)
+        var ref = storage.ref('img/' + fileName + '.jpg');
+        var file = qS('#imgUpload').files[0];
+        //Upload img to storage
+        ref.put(file).then(() => {
+            alert('Student succesfully added')
+            database.collection('students').doc(name + time).set({
+                name: name,
+                img: fileName,
+                group: groupRef,
+                hobby: hobby
+            })
+        });
+    })
 };
 
 function getDate(){
