ce2e022197f453a3991219bc2d60a77de523c098
Jacob Lundberg <jacob.lundberg@Jacobs-MacBook-Pro.local>
2019-08-22 10:01:25 +0200

    Added date generator + more inputfields

--- js/admin.js
+++ js/admin.js
@@ -14,16 +14,34 @@ const storage = firebase.storage();
 
 firebase.auth().signInAnonymously().catch(error => console.error(error));
 
-function loadFile(event) {
+function uploadFile(event) {
     var output = document.getElementById('previewImg');
-    output.src = URL.createObjectURL(event.target.files[0]);
+    output.src = URL.createObjectURL(qS('#imgUpload').files[0]);
 
-    var ref = storage.ref('img/hej.jpg');
+    //Get unique timeId
+    let time = getDate();
+
+
+
+    var ref = storage.ref('img/hej' + 'time' + '.jpg');
 
     var file = event.target.files[0];
     ref.put(file).then(function(snapshot) {
         console.log('Uploaded image!');
     });
 };
+
+function getDate(){
+    let time = new Date();
+    let date = "";
+    date += time.getFullYear();
+    date += time.getDate();
+    date += time.getMonth();
+    date += time.getHours();
+    date += time.getMinutes();
+    date += time.getSeconds();
+    date += time.getMilliseconds();
+    return date
+}
 
 
