60eeb20c8bbbd44287c2c75cdf2760bd202cda70
Jacob Lundberg <jacob.lundberg@Jacobs-MacBook-Pro.local>
2019-08-22 15:15:36 +0200

    Form/Group validation/creation

--- js/admin.js
+++ js/admin.js
@@ -27,8 +27,14 @@ function uploadFile() {
     let name = qS('input#studentname').value
     let group = qS('input#studentgroup').value
     let hobby = qS('textarea#studenthobby').value
-    let fileName = group.toUpperCase() + name.toLowerCase() + time
+    let file = qS('#imgUpload').files[0];
+
     let groupPath
+
+    //Form validation
+    if(name.length < 1 || group.length < 1 || file == undefined){
+        return
+    }
 
     //Find documents containing input group
     database.collection('groups').where('group', '==', `${group.toUpperCase()}`).get().then((snap) => {
@@ -37,19 +43,33 @@ function uploadFile() {
             groupPath = snapshot.id
         });
     }).then(() => {
+        //If no group is found
+        if(groupPath == undefined){
+            let action = confirm('The inputted group does not exist, would you like to create it?')
+            if(action){
+                database.collection('groups').doc().set({
+                    group: group
+                }, {merge: true})
+                uploadFile()
+                return
+            } else {
+                return
+            }
+        }
         //Create references to group document and image in storage
         groupRef = database.collection('groups').doc(groupPath)
         var ref = storage.ref('img/' + fileName + '.jpg');
-        var file = qS('#imgUpload').files[0];
+        
         //Upload img to storage
         ref.put(file).then(() => {
             alert('Student succesfully added')
+            let fileName = group.toUpperCase() + name.toLowerCase() + time
             database.collection('students').doc(name + time).set({
                 name: name,
                 img: fileName,
                 group: groupRef,
                 hobby: hobby
-            })
+            }, {merge: true})
         });
     })
 };
